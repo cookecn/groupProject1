b68332f39c608472d57c5e837e138730ff401041
Preston Sparks <mprestonsparks@gmail.com>
2019-03-11 17:27:48 -0500

    added functions to write trail data to table

--- map.js
+++ map.js
@@ -69,8 +69,7 @@ getUserIPLocation();
 
 // **************************************************
 
-https://www.hikingproject.com/data/get-trails?lat=40.0274&lon=-105.2519&maxDistance=10&key=YOUR_KEY_HERE
-
+// *** TO-DO: Update function to pull User's lat & long; currently hard-coded
 function getTrails() {
     var hikingProjectAPIKey = "200428466-2a448b50cc7ceff93b323bcffe658d58";
     var userLatitude = "35.8456"
@@ -81,10 +80,104 @@ function getTrails() {
         url: queryURL,
         method: "GET"
     }).then(function (response) {
-        // userCity = response.city;
-        // userLatitude = response.latitude;
-        // userLongitude = response.longitude;
+        var numberOfTrails = response.trails.length;
         console.log(response);
+        console.log("# of trails...",numberOfTrails);
+        newTable();
+        // var trailImage = response.trails[0].imgSqSmall;
+        // console.log("trailImage...",trailImage);
+        for (i=0; i<numberOfTrails; i++) {
+            var trailImage = response.trails[i].imgSqSmall;
+            var trailName = response.trails[i].name;
+            var trailRating = response.trails[i].stars;
+            var trailDifficulty = response.trails[i].difficulty;
+            var trailCondition = response.trails[i].conditionStatus;
+            addRow(trailImage,trailName,trailRating,trailDifficulty,trailCondition);
+        }
 })}
-// Call function
-getTrails();
+
+
+// **************************************************
+// Function to add trail data to page
+// Add new table to page
+function newTable() {
+    var newTableElement = $("<table>");
+    $(newTableElement).attr("id","results-table");
+    $("#table-body").append(newTableElement);
+
+    var tableHeaderRow = $("<tr>");
+    $(tableHeaderRow).attr("id","table-header-row");
+    $(newTableElement).append(tableHeaderRow);
+
+    var newImageHeader = $("<th>");
+    $(newImageHeader).attr("class","tableHeader")
+    $(newImageHeader).attr("id","table-image-header")
+    // $(newImageHeader).text();
+    $(tableHeaderRow).append(newImageHeader);
+
+    var newNameHeader = $("<th>");
+    $(newNameHeader).attr("class","tableHeader");
+    $(newNameHeader).attr("id","table-name-header");
+    $(newNameHeader).text("Trail Name");
+    $(tableHeaderRow).append(newNameHeader);
+
+    var newRatingHeader = $("<th>");
+    $(newRatingHeader).attr("class","tableHeader");
+    $(newRatingHeader).attr("id","table-rating-header");
+    $(newRatingHeader).text("Rating");
+    $(tableHeaderRow).append(newRatingHeader);
+    
+    var newDifficultyHeader = $("<th>");
+    $(newDifficultyHeader).attr("class","tableHeader");
+    $(newDifficultyHeader).attr("id","table-difficulty-header");
+    $(newDifficultyHeader).text("Difficulty");
+    $(tableHeaderRow).append(newDifficultyHeader);
+
+    var newConditionHeader = $("<th>");
+    $(newConditionHeader).attr("class","tableHeader");
+    $(newConditionHeader).attr("id","table-condition-header");
+    $(newConditionHeader).text("Current Condition");
+    $(tableHeaderRow).append(newConditionHeader);
+}
+
+function addRow(newImage,newName,newRating,newDifficulty,newConditionStatus) {
+    // Add new row to table
+    var newRow = $("<tr>")
+    $("#results-table").append(newRow);
+
+    // Add trail image to table row
+    var trailImage = $("<td>");
+    trailImage.attr("class","trailImage");
+    $(newRow).append(trailImage);
+    $(trailImage).append(newImage);
+
+    // Add Trail Name to table row
+    var trailName = $("<td>");
+    trailName.attr("class","trailName");
+    $(newRow).append(trailName);
+    $(trailName).text(newName);
+   
+    // Add Trail Rating to table row
+    var trailRating = $("<td>");
+    trailRating.attr("class","trailRating");
+    $(newRow).append(trailRating);
+    $(trailRating).text(newRating);
+
+    // Add Trail Difficulty to table row
+    var trailDifficulty = $("<td>");
+    trailDifficulty.attr("class","trailDifficulty");
+    $(newRow).append(trailDifficulty);
+    $(trailDifficulty).text(newDifficulty);
+
+    // Add Trail Condition Status to table row
+    var trailConditionStatus = $("<td>");
+    trailConditionStatus.attr("class","trailConditionStatus");
+    $(newRow).append(trailConditionStatus);
+    $(trailConditionStatus).text(newConditionStatus);
+  };
+
+// **************************************************
+// On click of "Find a Hike Near Me: Search" button
+$("#find-hike-button").click(function() {
+    getTrails();
+});
