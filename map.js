f2334c4ba957bd9467bdd47fe764c83f48f285a3
JASEpstein <jasepstein@gmail.com>
2019-03-13 19:18:52 -0500

    Adds Card Population

--- map.js
+++ map.js
@@ -1,3 +1,14 @@
+// *************************************************
+//Semantic UI Animation Scripts
+$('.special.cards .image').dimmer({
+    on: 'hover'
+});
+
+$('.ui.dropdown')
+    .dropdown();
+
+$('.ui.rating')
+    .rating();
 
 // **************************************************
 // IPGeolocation API (used to get geolocation of IP accessing the site)
@@ -6,142 +17,157 @@ var userCity;
 var userLatitude;
 var userLongitude;
 
-function getUserIPLocation() {
-    var ipGeoLocationAPIKey = "13254077d97f4249a0a6d6fd72053172";
-    var queryURL = "https://api.ipgeolocation.io/ipgeo?apiKey=" + ipGeoLocationAPIKey + "&fields=geo";
-    $.ajax({
-        url: queryURL,
-        method: "GET"
-    }).then(function (response) {
-        userCity = response.city;
-        userIPLatitude = response.latitude;
-        userIPLongitude = response.longitude;
-        console.log(response);
-        console.log("Lat: ",userIPLatitude);
-        console.log("Long: ",userIPLongitude);
-})}
+// function getUserIPLocation() {
+//     var ipGeoLocationAPIKey = "13254077d97f4249a0a6d6fd72053172";
+//     var queryURL = "https://api.ipgeolocation.io/ipgeo?apiKey=" + ipGeoLocationAPIKey + "&fields=geo";
+//     $.ajax({
+//         url: queryURL,
+//         method: "GET"
+//     }).then(function (response) {
+//         userCity = response.city;
+//         userIPLatitude = response.latitude;
+//         userIPLongitude = response.longitude;
+//         console.log(response);
+//         console.log("Lat: ", userIPLatitude);
+//         console.log("Long: ", userIPLongitude);
+//     })
+// }
 // Call function
-getUserIPLocation();
+// getUserIPLocation();
 
 // **************************************************
 
 // *** TO-DO: Update function to pull User's lat & long; currently hard-coded
 function getTrails() {
     var hikingProjectAPIKey = "200428466-2a448b50cc7ceff93b323bcffe658d58";
-    getUserIPLocation();
-    var userLatitude = userIPLatitude;
-    var userLongitude = userIPLongitude;
+    // getUserIPLocation();
+    // var userLatitude = userIPLatitude;
+    // var userLongitude = userIPLongitude;
     var maxDistance = "50" // Max distance in miles, default = 30, max = 200
-    var queryURL = "https://www.hikingproject.com/data/get-trails?lat=" + userLatitude + "&lon=" + userLongitude + "&maxDistance=" + maxDistance + "&key=" + hikingProjectAPIKey; 
-        $.ajax({
+    var queryURL = "https://www.hikingproject.com/data/get-trails?lat=" + "36.144700" + "&lon=" + "-86.804050" + "&maxDistance=" + maxDistance + "&key=" + hikingProjectAPIKey;
+    $.ajax({
         url: queryURL,
         method: "GET"
     }).then(function (response) {
         var numberOfTrails = response.trails.length;
         console.log(response);
-        newTable();
-        for (i=0; i<numberOfTrails; i++) {
-            var trailImage = response.trails[i].imgSqSmall;
-            var trailName = response.trails[i].name;
-            var trailRating = response.trails[i].stars;
-            var trailDifficulty = response.trails[i].difficulty;
-            var trailCondition = response.trails[i].conditionStatus;
-            if (trailImage === "") {
-                var placeHolderImage = 'images/trailplaceholder.jpg'
-                addRow(placeHolderImage,trailName,trailRating,trailDifficulty,trailCondition);
-                } else {
-                    addRow(trailImage,trailName,trailRating,trailDifficulty,trailCondition);
-                }
+        for (i = 0; i < numberOfTrails; i++) {
+            createNewCard(response);
+            // var trailImage = response.trails[i].imgSqSmall;
+            // var trailName = response.trails[i].name;
+            // var trailRating = response.trails[i].stars;
+            // var trailDifficulty = response.trails[i].difficulty;
+            // var trailCondition = response.trails[i].conditionStatus;
+            // if (trailImage === "") {
+            //     var placeHolderImage = 'images/trailplaceholder.jpg'
+            //     addRow(placeHolderImage, trailName, trailRating, trailDifficulty, trailCondition);
+            // } else {
+            //     addRow(trailImage, trailName, trailRating, trailDifficulty, trailCondition);
+            // }
         }
-})}
+    })
+}
 
+// $('#test-button').on('click', function () {
+//     getTrails();
+// });
 
 // **************************************************
 // Function to add trail data to page
 // Add new table to page
-function newTable() {
-    var newTableElement = $("<table>");
-    $(newTableElement).attr("id","results-table");
-    $("#table-body").append(newTableElement);
+// function newTable() {
+//     var newTableElement = $("<table>");
+//     $(newTableElement).attr("id", "results-table");
+//     $("#table-body").append(newTableElement);
 
-    var tableHeaderRow = $("<tr>");
-    $(tableHeaderRow).attr("id","table-header-row");
-    $(newTableElement).append(tableHeaderRow);
+//     var tableHeaderRow = $("<tr>");
+//     $(tableHeaderRow).attr("id", "table-header-row");
+//     $(newTableElement).append(tableHeaderRow);
 
-    var newImageHeader = $("<th>");
-    $(newImageHeader).attr("class","tableHeader")
-    $(newImageHeader).attr("id","table-image-header")
-    $(tableHeaderRow).append(newImageHeader);
+//     var newImageHeader = $("<th>");
+//     $(newImageHeader).attr("class", "tableHeader")
+//     $(newImageHeader).attr("id", "table-image-header")
+//     $(tableHeaderRow).append(newImageHeader);
 
-    var newNameHeader = $("<th>");
-    $(newNameHeader).attr("class","tableHeader");
-    $(newNameHeader).attr("id","table-name-header");
-    $(newNameHeader).text("Trail Name");
-    $(tableHeaderRow).append(newNameHeader);
+//     var newNameHeader = $("<th>");
+//     $(newNameHeader).attr("class", "tableHeader");
+//     $(newNameHeader).attr("id", "table-name-header");
+//     $(newNameHeader).text("Trail Name");
+//     $(tableHeaderRow).append(newNameHeader);
 
-    var newRatingHeader = $("<th>");
-    $(newRatingHeader).attr("class","tableHeader");
-    $(newRatingHeader).attr("id","table-rating-header");
-    $(newRatingHeader).text("Rating");
-    $(tableHeaderRow).append(newRatingHeader);
-    
-    var newDifficultyHeader = $("<th>");
-    $(newDifficultyHeader).attr("class","tableHeader");
-    $(newDifficultyHeader).attr("id","table-difficulty-header");
-    $(newDifficultyHeader).text("Difficulty");
-    $(tableHeaderRow).append(newDifficultyHeader);
+//     var newRatingHeader = $("<th>");
+//     $(newRatingHeader).attr("class", "tableHeader");
+//     $(newRatingHeader).attr("id", "table-rating-header");
+//     $(newRatingHeader).text("Rating");
+//     $(tableHeaderRow).append(newRatingHeader);
 
-    var newConditionHeader = $("<th>");
-    $(newConditionHeader).attr("class","tableHeader");
-    $(newConditionHeader).attr("id","table-condition-header");
-    $(newConditionHeader).text("Current Condition");
-    $(tableHeaderRow).append(newConditionHeader);
+//     var newDifficultyHeader = $("<th>");
+//     $(newDifficultyHeader).attr("class", "tableHeader");
+//     $(newDifficultyHeader).attr("id", "table-difficulty-header");
+//     $(newDifficultyHeader).text("Difficulty");
+//     $(tableHeaderRow).append(newDifficultyHeader);
+
+//     var newConditionHeader = $("<th>");
+//     $(newConditionHeader).attr("class", "tableHeader");
+//     $(newConditionHeader).attr("id", "table-condition-header");
+//     $(newConditionHeader).text("Current Condition");
+//     $(tableHeaderRow).append(newConditionHeader);
+// }
+
+// function addRow(newImage, newName, newRating, newDifficulty, newConditionStatus) {
+//     // Add new row to table
+//     var newRow = $("<tr>")
+//     $("#results-table").append(newRow);
+
+//     // Add trail image to table row
+//     var trailImage = $("<td>");
+//     trailImage.attr("class", "trailImage");
+//     var trailImageElement = $("<img>")
+//     $(trailImageElement).attr("src", newImage);
+//     $(trailImage).append(trailImageElement);
+//     $(newRow).append(trailImage);
+
+//     // Add Trail Name to table row
+//     var trailName = $("<td>");
+//     trailName.attr("class", "trailName");
+//     $(newRow).append(trailName);
+//     $(trailName).text(newName);
+
+//     // Add Trail Rating to table row
+//     var trailRating = $("<td>");
+//     trailRating.attr("class", "trailRating");
+//     $(newRow).append(trailRating);
+//     $(trailRating).text(newRating);
+
+//     // Add Trail Difficulty to table row
+//     var trailDifficulty = $("<td>");
+//     trailDifficulty.attr("class", "trailDifficulty");
+//     $(newRow).append(trailDifficulty);
+//     $(trailDifficulty).text(newDifficulty);
+
+//     // Add Trail Condition Status to table row
+//     var trailConditionStatus = $("<td>");
+//     trailConditionStatus.attr("class", "trailConditionStatus");
+//     $(newRow).append(trailConditionStatus);
+//     $(trailConditionStatus).text(newConditionStatus);
+// };
+
+// Add Cards
+function createNewCard(response) {
+    var newCard = $("<div class='red raised card' data-lat='" + response.trails[i].latitude + "' data-long='" + response.trails[i].longitude + "'><div class='blurring dimmable image'><div class='ui dimmer'><div class='content'><div class='center'><div class='ui inverted button'>Get Directions</div></div></div></div><div class='backgroundimg image' style='background-image: url(" + response.trails[i].imgMedium + ")'></div></div><div class='content'><h3>" + response.trails[i].name + "</h3><div class='meta'><span class='description'>" + response.trails[i].summary + "</span></div></div><div class='extra content'>Rating: <div class='ui star rating' data-rating='" + Math.round(response.trails[i].stars) + "'></div></div></div>");
+
+    $("#card-section").append(newCard);
+
 }
 
-function addRow(newImage,newName,newRating,newDifficulty,newConditionStatus) {
-    // Add new row to table
-    var newRow = $("<tr>")
-    $("#results-table").append(newRow);
-
-    // Add trail image to table row
-    var trailImage = $("<td>");
-    trailImage.attr("class","trailImage");
-    var trailImageElement = $("<img>")
-    $(trailImageElement).attr("src",newImage);
-    $(trailImage).append(trailImageElement);
-    $(newRow).append(trailImage);
-
-    // Add Trail Name to table row
-    var trailName = $("<td>");
-    trailName.attr("class","trailName");
-    $(newRow).append(trailName);
-    $(trailName).text(newName);
-   
-    // Add Trail Rating to table row
-    var trailRating = $("<td>");
-    trailRating.attr("class","trailRating");
-    $(newRow).append(trailRating);
-    $(trailRating).text(newRating);
-
-    // Add Trail Difficulty to table row
-    var trailDifficulty = $("<td>");
-    trailDifficulty.attr("class","trailDifficulty");
-    $(newRow).append(trailDifficulty);
-    $(trailDifficulty).text(newDifficulty);
-
-    // Add Trail Condition Status to table row
-    var trailConditionStatus = $("<td>");
-    trailConditionStatus.attr("class","trailConditionStatus");
-    $(newRow).append(trailConditionStatus);
-    $(trailConditionStatus).text(newConditionStatus);
-  };
 
 
 // **************************************************
 // On click of "Find a Hike Near Me: Search" button
-$("#find-hike-button").click(function() {
-    $("#table-body").empty(); // Empty tabe-body to prevent multiple button clicks from re-displaying table data
+$("#find-hike-button").click(function () {
+    // $("#table-body").empty(); // Empty tabe-body to prevent multiple button clicks from re-displaying table data
     getTrails();
+    $('#selection-box').hide();
 });
 
 // **************************************************
@@ -153,28 +179,38 @@ $("#find-hike-button").click(function() {
 // Shows how to create loop to add markers
 
 var map;
-function initMap(){
+
+function initMap() {
     // Map otions
     var options = {
-        center: {lat:35.8456,lng:-86.3903},
-        zoom:10,
+        center: {
+            lat: 35.8456,
+            lng: -86.3903
+        },
+        zoom: 10,
     }
-// New map
-map = new google.maps.Map(document.getElementById('directions-map'), options); 
+    // New map
+    map = new google.maps.Map(document.getElementById('directions-map'), options);
 
-// Add marker for User's current location
-addMarker({lat:35.9828,lng:-86.5186});
-
-// Add marker for trail selected
-addMarker({lat:35.8456,lng:-86.3903})
-
-// Function to add new markers on map
-function addMarker(coords) {
-    new google.maps.Marker({
-        position:coords,
-        map:map,
-        icon:'images/hiker-icon.png'
+    // Add marker for User's current location
+    addMarker({
+        lat: 35.9828,
+        lng: -86.5186
     });
+
+    // Add marker for trail selected
+    addMarker({
+        lat: 35.8456,
+        lng: -86.3903
+    })
+
+    // Function to add new markers on map
+    function addMarker(coords) {
+        new google.maps.Marker({
+            position: coords,
+            map: map,
+            icon: 'images/hiker-icon.png'
+        });
     }
 }
 
@@ -201,10 +237,11 @@ function addMarker(coords) {
 // TO-DO: "center", "start", and "end" are still hardcoded- replace values w/ userLocation and trail selected ******
 var directionsDisplay;
 var directionsService;
+
 function getDirections() {
     directionsDisplay = new google.maps.DirectionsRenderer();
     directionsService = new google.maps.DirectionsService();
-    var chicago = new google.maps.LatLng(41.850033,-87.6500523);
+    var chicago = new google.maps.LatLng(41.850033, -87.6500523);
     var mapOtions = {
         zoom: 7,
         center: chicago
@@ -216,14 +253,14 @@ function getDirections() {
 }
 
 function calcRoute() {
-    var start = new google.maps.LatLng(41.850033,-87.6500523);
-    var end = new google.maps.LatLng(41.850033,-87.7000523)
+    var start = new google.maps.LatLng(41.850033, -87.6500523);
+    var end = new google.maps.LatLng(41.850033, -87.7000523)
     var request = {
         origin: start,
         destination: end,
         travelMode: google.maps.TravelMode.DRIVING
     };
-    directionsService.route(request, function(result, status) {
+    directionsService.route(request, function (result, status) {
         if (status == google.maps.DirectionsStatus.OK) {
             directionsDisplay.setDirections(result);
         }
